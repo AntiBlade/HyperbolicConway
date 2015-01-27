cf80c393c0fa93b71a304909b3bf7788c578617c
Jane <jellolegs1@gmail.com>
2015-01-26 22:02:49 -0600

    HyperConway.py

--- HyperConway.py
+++ HyperConway.py
@@ -1,7 +1,4 @@
-import networkx as nx
 import itertools as iter
-
-f = open('result.txt','w')
 
 """
 The next three lines simply set up some initial stuff; finding all permutations
@@ -9,26 +6,28 @@ of 1, 2, 3, 4 and storing them, making a list of labels a-x, and then combinging
 them to make a dictionary that associates every letter with a permutation.
 """
 
-permList = [list(i) for i in list(iter.permutations([1,2,3,4,]))]
-permLabels = {i : j for i, j in iter.izip(range(24), permList)}
-
+permLis = [list(i) for i in list(iter.permutations([5,1,2,3,4,]))]
+permList = []
+for z in permLis:
+    if z[0] == 5:
+        permList.append(z)
+permLabels = dict(enumerate(permList))
 """
 Here are the functions for generating the neighbors of a permutation; I tried 
 splitting up the work as much as possible so that if my algorithms are wrong you
 can modify them relatively easily (hence the 'swap' 'fronttoback' etc.)
 """
-
-def swap(List, indexA, indexB):
-    List[indexA], List[indexB] = List[indexB], List[indexA]
-    return List
+def normalize(p,q):
+    p = p[p.index(q):] + p[:p.index(q)]
+    return p
     
-def frontToBack(List):
-    List.append(List.pop(0))
-    return List
+def rotateLeft(P,k):
+    P = P[k:] + P[:k]
+    return P
     
-def backToFront(List):
-    List.insert(0, List.pop())
-    return List
+def swap(L,k):
+    L = L[:k] + [L[k+1],L[k]] + L[k+2:]
+    return L
 
 def getLabel(dictionary, search):
     for key, value in dictionary.iteritems():
@@ -39,28 +38,10 @@ def makeNeighbors(perm):
     neighborList = [] 
     neighborLabels = [] 
     
-    for i in range(len(perm)):
-        neighbor = perm[:]
-        if i == (len(perm)-1):
-            neighborList.append(backToFront(neighbor))
-            neighbor = perm[:]
-            neighborList.append(frontToBack(neighbor))
-        else:
-            neighborList.append(swap(neighbor, i, i+1))
-        
-    currentTotal = len(neighborList)-1
-            
-    for i in range(0, currentTotal): 
-        listCopy = neighborList[:]
-        doubleSwap = listCopy[i][:]
-        if i == 2:
-            neighborList.append(backToFront(doubleSwap))   
-        elif i == 3:
-            neighborList.append(swap(doubleSwap, 0, 1))
-            doubleSwap = listCopy[i][:]
-            neighborList.append(backToFront(doubleSwap))
-        elif i <= 1:
-            neighborList.append(swap(doubleSwap, i+1, i+2))
+    for k in range(5):
+        neighborList.append(normalize(swap(rotateLeft(perm,k),0),5))
+        j = swap(rotateLeft(perm,k),0)
+        neighborList.append(normalize(swap(j,2),5))
     
     for neighbors in neighborList:
         neighborLabels.append(getLabel(permLabels, neighbors))
@@ -75,19 +56,7 @@ and making a huge list of every possible initial state. Also here is the Conway
 life/death algorithm itself, which will be run once on each initial state.
 """
 
-bitStrings = ["".join(seq) for seq in iter.product("01", repeat = 24)]
-bitStrings = bitStrings[8388608:12582912]
-bitStrings = tuple(bitStrings)
-
-"""
-Copy the entire elif block below in the Conway function, and make sure the indentation is correct and consistent.
-change the condition (elif i == something) to match the appropriate position you're checking (essentially
-increase it by one each time you paste). Now, using the two dictionaries that are printed, change 
-the bracketed numbers in variable total to match the numbers that correspond to the letters
-in the neighbors lists. 
-"""
-
-def resultCalc(List, pos):
+def liveNeighbors(List, pos):
     global allNeighbors
     total = 0
     for i in allNeighbors[pos]:
@@ -98,11 +67,12 @@ def Conway(bitNum):
     result = []
     bitNum = [int(i) for i in list(bitNum)]
     for i in range(len(bitNum)):
-        if (bitNum[i] == 1 and (resultCalc(bitNum, i) == 2 or resultCalc(bitNum, i) ==3)) or (bitNum[i] == 0 and resultCalc(bitNum, i) == 3):
+        if (bitNum[i] == 1 and (liveNeighbors(bitNum, i) == 2 or liveNeighbors(bitNum, i) ==3)) or (bitNum[i] == 0 and liveNeighbors(bitNum, i) == 3):
             result.append(1)
         else: 
             result.append(0)
     return result
+    
 
 """
 This part does all of the actual work you care about; it takes every 24-bit 
@@ -113,26 +83,27 @@ printed so you can see the lengths of every single cycle (considering there are
 all duplicate cycles, but this will require active checking of what particular
 cycles have been attained with each new starting value.  
 """
-
-for j in bitStrings:
-    conwayGraph = nx.DiGraph()
-
-    p1 = tuple([int(i) for i in list(j)])
-    p2 = tuple(Conway(list(p1)))
-    
-    f.write('Starting String: ' + str(p1) + '\n')
-    f.write('Number Alive: ' + str(sum(p1)) + '\n')
-    
-
-    for i in range(1, 100): 
-        conwayGraph.add_node(p1)
-        conwayGraph.add_node(p2)
-        conwayGraph.add_edge(p1, p2)
-        p1 = p2
-        p2 = tuple(Conway(list(p2)))
-        
-    cycles = list(nx.simple_cycles(conwayGraph))
-    f.write('Head Length: ' + str(len(conwayGraph)-len(cycles[0])) + '\n')
-    f.write('Tail Length:' + str(len(cycles[0])))
-    f.write('\n')
-    f.write('\n')
+if __name__ == "__main__":
+    bitStrings = ["".join(seq) for seq in iter.product("01", repeat = 24)]
+    #bitStrings = bitStrings[8388608:12582912]
+    bitStrings = tuple(bitStrings)
+    f = open('result.txt','w')
+    f.write('[Number,StartingConfig,NumberAlive,HeadLength,TailLength' + '\n')
+    start = 1
+    for j in bitStrings:
+        starting = [int(i) for i in list(j)]
+        seen = [starting]
+        p1 = tuple([int(i) for i in list(j)])
+        f.write('[#' + str(start) + ',')
+        start += 1
+        f.write(str(p1) + ',')
+        f.write(str(sum(p1)) + ',')
+        while True:
+            j = Conway(j)
+            if j in seen:
+                f.write(str(len(seen[:seen.index(j)])) + ',')
+                f.write(str(len(seen[seen.index(j):])))
+                f.write(']' + '\n')
+                break
+            else:
+                seen.append(j)
